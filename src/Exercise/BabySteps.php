90904fccb4c0a6bc2a33b7565865ae739faa0803
Aydin Hassan <aydin@hotmail.co.uk>
2015-09-05 19:58:50 +0100

    Baby steps exercise

---
+++ src/Exercise/BabySteps.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PhpWorkshop\PhpWorkshop\Exercise;
+
+use PhpWorkshop\PhpWorkshop\ExerciseCheck\StdOutCheck;
+
+/**
+ * Class BabySteps
+ * @package PhpWorkshop\PhpWorkshop\Exercise
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class BabySteps implements ExerciseInterface, StdOutCheck
+{
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Baby Steps';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'NASA Level Programming';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSolution()
+    {
+        return __DIR__ . '/../../res/solutions/baby-steps/solution.php';
+    }
+
+    /**
+     * @return array
+     */
+    public function getArgs()
+    {
+        $numArgs = rand(0, 10);
+
+        $args = [];
+        for ($i = 0; $i < $numArgs; $i ++) {
+            $args[] = rand(0, 100);
+        }
+
+        return $args;
+    }
+}
--- test.php
+++ test.php
@@ -3,11 +3,13 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpWorkshop\PhpWorkshop\Comparator\StdOut;
+use PhpWorkshop\PhpWorkshop\Exercise\BabySteps;
 use PhpWorkshop\PhpWorkshop\Exercise\HelloWorld;
 use PhpWorkshop\PhpWorkshop\ExerciseRunner;
 
 $runner = new ExerciseRunner(new StdOut);
 
+echo "====HELLO WORLD====\n\n";
 $helloWorld = new HelloWorld;
 
 echo "____FAIL___\n\n";
@@ -28,3 +30,23 @@ var_dump($result->isSuccessful());
 echo "Errors\n";
 var_dump($result->getErrors());
 
+echo "====HELLO WORLD====\n\n";
+$babySteps = new BabySteps;
+
+echo "____FAIL___\n\n";
+$result = $runner->runExercise($babySteps, __DIR__ . '/test/res/baby-steps-fail.php');
+
+echo "Successful?\n";
+var_dump($result->isSuccessful());
+
+echo "Errors\n";
+var_dump($result->getErrors());
+
+echo "____PASS___\n\n";
+$result = $runner->runExercise($babySteps, __DIR__ . '/test/res/baby-steps-pass.php');
+
+echo "Successful?\n";
+var_dump($result->isSuccessful());
+
+echo "Errors\n";
+var_dump($result->getErrors());
