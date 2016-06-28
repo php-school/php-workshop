700284066e58660f8d67b83fe8b78df0298663f2
Aydin Hassan <aydin@hotmail.co.uk>
2016-06-29 00:13:47 +0200

    output docs

--- src/Output/OutputInterface.php
+++ src/Output/OutputInterface.php
@@ -13,41 +13,57 @@ use Zend\Diactoros\Request;
 interface OutputInterface
 {
     /**
+     * Write a string as an error. Should be decorated in someway
+     * which highlights the severity.
+     *
      * @param string $error
      */
     public function printError($error);
 
     /**
+     * Write a string to the output.
+     *
      * @param string $content
      */
     public function write($content);
 
     /**
+     * Write an array of strings, each on a new line.
+     *
      * @param array $lines
      */
     public function writeLines(array $lines);
 
     /**
+     * Write a string terminated with a newline.
+     *
      * @param string $line
      */
     public function writeLine($line);
 
     /**
-     * Write empty line
+     * Write an empty line.
      */
     public function emptyLine();
 
     /**
+     * Write a line break.
+     *
      * @return string
      */
     public function lineBreak();
 
     /**
+     * Write a title section. Should be decorated in a way which makes
+     * the title stand out.
+     *
      * @param string $title
      */
     public function writeTitle($title);
 
     /**
+     * Write a PSR-7 request.
+     *
      * @param RequestInterface $request
      */
     public function writeRequest(RequestInterface $request);
