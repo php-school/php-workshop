01942810ceb0515b09063daa3b2976b8d063c966
Aydin Hassan <aydin@hotmail.co.uk>
2016-07-02 16:25:50 +0200

    CS

--- src/Result/ResultInterface.php
+++ src/Result/ResultInterface.php
@@ -4,7 +4,8 @@ namespace PhpSchool\PhpWorkshop\Result;
 
 /**
  * The base result interface, on it's own does not mean much, instead the
- * interfaces `PhpSchool\PhpWorkshop\Result\SuccessInterface` & `PhpSchool\PhpWorkshop\Result\FailureInterface` should be used.
+ * interfaces `PhpSchool\PhpWorkshop\Result\SuccessInterface` & `PhpSchool\PhpWorkshop\Result\FailureInterface`
+ * should be used.
  *
  * @package PhpSchool\PhpWorkshop\Result
  */
