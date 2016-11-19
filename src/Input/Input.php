d0587ce6ceded1f4aac702f99acd1e677e8e7109
Aydin Hassan <aydin@hotmail.co.uk>
2016-11-19 22:10:29 +0100

    Introduce Input object

---
+++ src/Input/Input.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace PhpSchool\PhpWorkshop\Input;
+
+use PhpSchool\PhpWorkshop\CommandDefinition;
+use PhpSchool\PhpWorkshop\Exception\InvalidArgumentException;
+
+/**
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class Input
+{
+    /**
+     * @var string
+     */
+    private $appName;
+
+    /**
+     * @var array
+     */
+    private $arguments = [];
+
+    /**
+     * Input constructor.
+     * @param $appName
+     * @param array $arguments
+     */
+    public function __construct($appName, array $arguments = [])
+    {
+        $this->appName = $appName;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppName()
+    {
+        return $this->appName;
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasArgument($name)
+    {
+        return isset($this->arguments[$name]);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    public function getArgument($name)
+    {
+        if (!$this->hasArgument($name)) {
+            throw new InvalidArgumentException(sprintf('Argument with name: "%s" does not exist', $name));
+        }
+
+        return $this->arguments[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     */
+    public function setArgument($name, $value)
+    {
+        $this->arguments[$name] = $value;
+    }
+}
