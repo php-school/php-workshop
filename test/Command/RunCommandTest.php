a768bb0555bce2fe89e0f7d8a16990d414eb34df
Aydin Hassan <aydin@hotmail.co.uk>
2016-11-25 10:25:01 +0100

    Run Command tests

---
+++ test/Command/RunCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PhpSchool\PhpWorkshopTest\Command;
+
+use Colors\Color;
+use PhpSchool\CliMenu\Terminal\TerminalInterface;
+use PhpSchool\PhpWorkshop\Command\RunCommand;
+use PhpSchool\PhpWorkshop\ExerciseDispatcher;
+use PhpSchool\PhpWorkshop\ExerciseRepository;
+use PhpSchool\PhpWorkshop\Input\Input;
+use PhpSchool\PhpWorkshop\Output\StdOutput;
+use PhpSchool\PhpWorkshop\UserState;
+use PhpSchool\PhpWorkshopTest\Asset\CliExerciseImpl;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class RunCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $input = new Input('appName', ['program' => 'solution.php']);
+
+        $exercise = new CliExerciseImpl;
+        $repo = new ExerciseRepository([$exercise]);
+
+        $state = new UserState;
+        $state->setCurrentExercise('my-exercise');
+        $color = new Color;
+        $color->setForceStyle(true);
+        $output = new StdOutput($color, $this->createMock(TerminalInterface::class));
+
+        $dispatcher = $this->prophesize(ExerciseDispatcher::class);
+        $dispatcher->run($exercise, $input, $output)->shouldBeCalled();
+
+        $command = new RunCommand($repo, $dispatcher->reveal(), $state, $output);
+        $command->__invoke($input);
+    }
+}
