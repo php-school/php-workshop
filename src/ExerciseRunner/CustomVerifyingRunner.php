f8b96bfd510f673cbecf6116c8c4aa33e7999669
Aydin Hassan <aydin@hotmail.co.uk>
2016-11-28 21:02:23 +0100

    Update doc blocks

--- src/ExerciseRunner/CustomVerifyingRunner.php
+++ src/ExerciseRunner/CustomVerifyingRunner.php
@@ -46,14 +46,8 @@ class CustomVerifyingRunner implements ExerciseRunnerInterface
     }
 
     /**
-     * Verify a solution to an exercise. Verification involves executing the reference solution
-     * and the student's solution and comparing their output. If the output is the same
-     * an instance of `PhpSchool\PhpWorkshop\Result\SuccessInterface` should be returned, if the output
-     * is not the same, or something else went wrong then an instance of
-     * `\PhpSchool\PhpWorkshop\Result\FailureInterface` should be returned.
-     *
-     * Other things that could go wrong include the student's solution returning a non-zero
-     * exit code, or a notice/warning being exhibited.
+     * Delegate to the exercise for verifying. Verifying could mean checking that a program was installed or that some
+     * other arbitrary task was performed.
      *
      * @param Input $input The command line arguments passed to the command.
      * @return ResultInterface The result of the check.
@@ -64,9 +58,8 @@ class CustomVerifyingRunner implements ExerciseRunnerInterface
     }
 
     /**
-     * Run a solution to an exercise. This simply run's the student's solution with the correct input from the exercise
-     * (such as the CLI arguments) and prints the output directly. This allows the student to have the environment
-     * setup for them including getting a different set of arguments each time (if the exercise supports that).
+     * Running a custom verifying exercise does nothing. There is no program required, therefore there is nothing
+     * to run.
      *
      * @param Input $input The command line arguments passed to the command.
      * @param OutputInterface $output A wrapper around STDOUT.
