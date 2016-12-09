d012e0d750c6dc7943872c74d2ff6b7e9a1b8fd8
Aydin Hassan <aydin@hotmail.co.uk>
2016-12-09 22:00:32 +0100

    Generic comparison failure result

---
+++ test/ResultRenderer/ComparisonFailureRendererTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PhpSchool\PhpWorkshopTest\ResultRenderer;
+
+use PhpSchool\PhpWorkshop\Result\ComparisonFailure;
+use PhpSchool\PhpWorkshop\ResultRenderer\ComparisonFailureRenderer;
+
+/**
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
+ */
+class ComparisonFailureRendererTest extends AbstractResultRendererTest
+{
+    public function testRender()
+    {
+        $failure  = new ComparisonFailure('Name', 'EXPECTED OUTPUT', 'ACTUAL OUTPUT');
+        $renderer = new ComparisonFailureRenderer($failure);
+
+        $expected  = "  \e[33m\e[1mYOUR OUTPUT:\e[0m\e[0m\n";
+        $expected .= "  \e[31m\"ACTUAL OUTPUT\"\e[0m\n\n";
+        $expected .= "  \e[33m\e[1mEXPECTED OUTPUT:\e[0m\e[0m\n";
+        $expected .= "  \e[32m\"EXPECTED OUTPUT\"\e[0m\n";
+
+        $this->assertEquals($expected, $renderer->render($this->getRenderer()));
+    }
+}
