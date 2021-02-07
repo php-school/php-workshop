fdca3a01b0939d9bcd215cd2294b03c603216157
Aydin Hassan <aydin@hotmail.co.uk>
2021-02-07 21:09:27 +0100

    CS

--- src/ExerciseCheck/FileComparisonExerciseCheck.php
+++ src/ExerciseCheck/FileComparisonExerciseCheck.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace PhpSchool\PhpWorkshop\ExerciseCheck;
 
 /**
- * This interface should be implemented when you require the check `\PhpSchool\PhpWorkshop\Check\FileComparisonCheck` in your
- * exercise.
+ * This interface should be implemented when you require the check `\PhpSchool\PhpWorkshop\Check\FileComparisonCheck`
+ * in your exercise.
  */
 interface FileComparisonExerciseCheck
 {
